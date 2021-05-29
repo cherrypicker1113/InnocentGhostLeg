c5976e2d276ade69ce0b1917c6d3f0e0edf0b83b
권해련 <hrgwon@miridih.com>
2021-05-30 02:21:44 +0900

    issue-13 액티비티 생성

---
+++ app/src/main/java/com/innocent/ghostleg/HrgwonActivity.kt
@@ -0,0 +1,11 @@
+package com.innocent.ghostleg
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class HrgwonActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hrgwon)
+    }
+}
