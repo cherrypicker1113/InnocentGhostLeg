f7b000c58b7898b42af9160f45734dd6d33ed646
권해련 <hrgwon@miridih.com>
2021-06-03 01:31:10 +0900

    issue-13 recyclerview 추가 GridView 활용

--- app/src/main/java/com/innocent/ghostleg/HrgwonActivity.kt
+++ app/src/main/java/com/innocent/ghostleg/HrgwonActivity.kt
@@ -1,14 +1,12 @@
 package com.innocent.ghostleg
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
 import com.innocent.ghostleg.databinding.ActivityHrgwonBinding
 
 class HrgwonActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHrgwonBinding
     private val maxNumber: Int = 20
     private var num: Int = 5
-    private val textList: ArrayList<TextView> = arrayListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,38 +16,30 @@ class HrgwonActivity : AppCompatActivity() {
             finish()
         }
 
+        val adapter = HrgwonAdapter(this, arrayListOf());
+        binding.gridView.adapter = adapter
+
         num = Integer.parseInt(binding.tvNum.text.toString())
         for(i in 0 until num) {
-            addTextView()
+            adapter.addItem(Data(adapter.getCount() + 1, "이름"))
         }
 
         // 푸시가 잘 되는지 확인하기 위한 주석
         binding.btnDecrease.setOnClickListener {
-            println("num은 $num , textList size는 $textList.size")
             if (checkValid(num - 1)) {
                 updateNumber(num - 1)
-                binding.textListLinearLayout.removeView(textList.last());
-                textList.remove(textList.last())
+                adapter.removeItem()
             }
         }
 
         binding.btnIncrease.setOnClickListener {
-            println("num은 $num , textList size는 $textList.size")
             if (checkValid(num + 1)) {
                 updateNumber(num + 1)
-                addTextView()
+                adapter.addItem(Data(adapter.getCount() + 1, "이름"))
             }
         }
 
         setContentView(binding.root)
-    }
-
-    private fun addTextView() {
-        val textView = TextView(this)
-        textList.add(textView);
-        textView.text = "${textList.lastIndex + 1}. TEXT_VIEW"
-        binding.textListLinearLayout.addView(textView)
-
     }
 
     private fun updateNumber(value: Int) {
